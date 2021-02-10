1d983636aca905add77f333883bb94eab5384943
Bipeen1 <atifaslam8652739490@gmail.com>
2021-02-11 00:14:42 +0530

    Fragment add working Properly

--- app/src/main/java/com/example/quizzical/fragment/AddSubjectFragment.java
+++ app/src/main/java/com/example/quizzical/fragment/AddSubjectFragment.java
@@ -10,11 +10,15 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.quizzical.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AddSubjectFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.add_subject_fragment, container, false);
+
+
     }
 }
