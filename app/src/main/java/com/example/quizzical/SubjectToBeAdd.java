50e17e36581aba224e43b9c6a880744dc2c16827
Bipeen1 <atifaslam8652739490@gmail.com>
2021-02-12 00:35:12 +0530

    Add Subject Fragment Working Properly

---
+++ app/src/main/java/com/example/quizzical/SubjectToBeAdd.java
@@ -0,0 +1,42 @@
+package com.example.quizzical;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SubjectToBeAdd extends AppCompatActivity {
+
+    Button addSubject;
+    EditText subjectName;
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject_to_be);
+
+        addSubject = (Button) findViewById(R.id.submit_subject);
+        subjectName = (EditText) findViewById(R.id.subject_name);
+
+        addSubject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String subject = subjectName.getText().toString().trim();
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("Subjects");
+                reference.child(subject).setValue(subject);
+                Toast.makeText(SubjectToBeAdd.this, "subject Added", Toast.LENGTH_SHORT).show();
+                subjectName.setText("");
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/quizzical/fragment/AddSubjectFragment.java
+++ app/src/main/java/com/example/quizzical/fragment/AddSubjectFragment.java
@@ -1,24 +1,92 @@
 package com.example.quizzical.fragment;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.quizzical.R;
+import com.example.quizzical.SubjectToBeAdd;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import static android.content.ContentValues.TAG;
 
 public class AddSubjectFragment extends Fragment {
+    FloatingActionButton floatingActionButton;
+    ListView myListView;
+
+    DatabaseReference subjectRef;
+
+    ArrayList<String> myArrayList = new ArrayList<>();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.add_subject_fragment, container, false);
+        View view = inflater.inflate(R.layout.add_subject_fragment, container, false);
+        floatingActionButton = view.findViewById(R.id.fab);
+        myListView = view.findViewById(R.id.listview1);
+        ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, myArrayList);
+        myListView.setAdapter(myArrayAdapter);
 
+        subjectRef = FirebaseDatabase.getInstance().getReference("Subjects");
+        subjectRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String value = snapshot.getValue(String.class);
+                myArrayList.add(value);
+                myArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                myArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), SubjectToBeAdd.class);
+                startActivity(intent);
+            }
+        });
+        return view;
 
     }
+
+
 }
+
